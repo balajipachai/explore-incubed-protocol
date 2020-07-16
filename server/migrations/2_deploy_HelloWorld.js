af2d3c32bcb54d11b0d01b177af20045d8afb284
balajipachai <balaji.pachai08@gmail.com>
2020-07-16 16:24:02 +0530

    Added migration files

--- server/migrations/2_deploy_HelloWorld.js
+++ server/migrations/2_deploy_HelloWorld.js
@@ -1,6 +1,8 @@
-const HelloWorld = artifacts.require('HelloWorld')
+/* eslint-disable func-names */
+// eslint-disable-next-line no-undef
+const HelloWorld = artifacts.require('HelloWorld');
 
 module.exports = function (deployer, network, accounts) {
-    const owner = accounts[0]
-    deployer.deploy(HelloWorld, { from: owner });
-}
+  const [owner] = accounts;
+  deployer.deploy(HelloWorld, { from: owner });
+};
---
+++ server/migrations/3_deploy_AllContracts.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable func-names */
+
+const BlockhashRegistry = artifacts.require('BlockhashRegistry');
+const NodeRegistryData = artifacts.require('NodeRegistryData');
+const NodeRegistryLogic = artifacts.require('NodeRegistryLogic');
+
+module.exports = function (deployer, network, accounts) {
+  const [owner] = accounts;
+  const minDeposit = '0x2386f26fc10000';
+  deployer.deploy(BlockhashRegistry, { from: owner })
+    .then(() => deployer.deploy(NodeRegistryData, { from: owner }))
+    .then(() => deployer.deploy(
+      NodeRegistryLogic, BlockhashRegistry.address, NodeRegistryData.address, minDeposit,
+      { from: owner },
+    ));
+};
